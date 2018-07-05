7036fe17b84b06bba386ed60641e49c434d2ff9b
Hemant <hemantiitkcse@gmail.com>
2018-07-06 02:21:29 +0530

    first commit

--- server.js
+++ server.js
@@ -34,7 +34,7 @@
 const db = require('knex')({
   client : 'mysql',
   connection: {
-    host : 'quikcure.mysql.database.azure.com',,
+    host : 'quikcure.mysql.database.azure.com',
     user : 'serveradmin@quikcure',
     password : 'Utsav@123',
     ssl: true,
